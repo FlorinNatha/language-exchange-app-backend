a28707be8e947faad2b683ce0a3626e1478ed176
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-20 19:50:33 +0530

    updated backend folder stucture

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const socketHandler = require('./sockets/socketHandler');
 
 dotenv.config();
-connectDB();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+// Connect DB
+require('./config/db')();
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+// ... other routes
 
+// Start server
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server, { cors: { origin: "*" } });
+const io = socketio(server, { cors: { origin: "*" } });
+require('./sockets/socketHandler')(io);
 
-socketHandler(io);
-
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
