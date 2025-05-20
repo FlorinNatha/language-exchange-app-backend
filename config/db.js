f5deeafdca1782286d32c0aad064963ee32a6706
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-21 00:21:57 +0530

    updated databse connection

--- config/db.js
+++ config/db.js
@@ -4,12 +4,12 @@ const ConnectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/chatterly', {
       useNewUrlParser: true,
-      userUnifiedTopology: true,
+      
     });
     console.log('MongoDB connected');
   } catch(err) {
     console.error(err.message);
-    proq.exit(1);
+    process.exit(1);
   }
 };
 module.exports = ConnectDB;
