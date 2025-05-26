c76316e2f2d268e722866912d68613f1276c1a16
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-26 23:31:34 +0530

    updated authentication middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User')
 
-const protect = (req, res, next) => {
+const protect = async(req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "No token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const user = await User.findById(decoded.id).select('-password');
+    if (!user) return res.status(401).json({message: "User not found"});
+
+    req.user = user;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
