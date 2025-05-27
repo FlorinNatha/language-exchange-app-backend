c263ae8f1dfb1a02a768b9f4e2c7db8f7814421c
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-27 23:33:51 +0530

    user management

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,28 @@
 const User = require('../models/User');
 
-const getAllUsers = async (req, res) => {
-  const users = await User.find().select('-password');
-  res.json(users);
+// 1. Get All Users (excluding passwords)
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({msg: err.message});
+  }
 };
 
-module.exports = { getAllUsers };
+//2. Get User Profile
+exports.getUserProfile = async (req, res) => {
+  try{
+    const user = await User.findById(req.params.id).select('-password');
+    if(!user) return res.status(404).json({msg: 'User not found'});
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ msg: err.message});
+  }
+}
+
+//3. Update User Profile
+exports.updateUserProfile = async (req, res) => {
+  
+}
